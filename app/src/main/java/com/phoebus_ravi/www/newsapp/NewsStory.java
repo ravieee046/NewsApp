7436d738096f21681dac74fe4b26bb8297b40638
Ravi Gupta <ravik.rkg@gmail.com>
2017-11-01 19:35:20 +0530

    Add Login Page and News Page

---
+++ app/src/main/java/com/phoebus_ravi/www/newsapp/NewsStory.java
@@ -0,0 +1,38 @@
+package com.phoebus_ravi.www.newsapp;
+
+public class NewsStory {
+    private String mTitle;
+    private String mSection;
+    private String mPublishedDate;
+    private String mWebUrl;
+    private String mType;
+
+    public NewsStory(String mTitle, String mSection, String mPublishedDate, String mWebUrl, String mType) {
+        this.mTitle = mTitle;
+        this.mSection = mSection;
+        this.mPublishedDate = mPublishedDate;
+        this.mWebUrl = mWebUrl;
+        this.mType = mType;
+
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSection() {
+        return mSection;
+    }
+
+    public String getPublishedDate() {
+        return mPublishedDate;
+    }
+
+    public String getWebUrl() {
+        return mWebUrl;
+    }
+
+    public String getType() {
+        return mType;
+    }
+}
---
+++ app/src/main/java/com/phoebus_ravi/www/newsapp/NewsStoryAsyncTaskLoader.java
@@ -0,0 +1,26 @@
+package com.phoebus_ravi.www.newsapp;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class NewsStoryAsyncTaskLoader extends AsyncTaskLoader<List<NewsStory>> {
+
+    private String[] mUrl;
+
+    public NewsStoryAsyncTaskLoader(Context context, String... mUrl) {
+        super(context);
+        this.mUrl = mUrl;
+    }
+
+    @Override
+    public List<NewsStory> loadInBackground() {
+        return QueryUtils.fetchNewsStoryData(mUrl[0]);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+}
